6e43cf9c4531fe6b5c326e99930ca282db15f2e0
johnsonprince664-web <johnsonprince664@gmail.com>
2026-08-17 19:57:24 -0400

    Load voice-only JARVIS mode before dashboard interactions

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./jarvisUpgrades";
 import "./orbVoiceEnhancements";
+import "./voiceOnlyMode";
 import "./computerControl";
 import "./connectedIntegrations";
 import "./pushSetup";
