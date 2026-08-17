8027a14b2a1455215a9c97f344111e9d3fd88b7e
johnsonprince664-web <johnsonprince664@gmail.com>
2026-08-17 19:49:48 -0400

    Load audio-reactive orb and upgraded JARVIS voice

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./jarvisUpgrades";
+import "./orbVoiceEnhancements";
 import "./computerControl";
 import "./connectedIntegrations";
 import "./pushSetup";
