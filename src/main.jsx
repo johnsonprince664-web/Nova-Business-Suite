f5a53beed2447676eefccdff5b8c944c7587af29
johnsonprince664-web <johnsonprince664@gmail.com>
2026-08-17 19:26:48 -0400

    Load JARVIS upgrade runtime before the dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./jarvisUpgrades";
 import App from "./App";
 import CRMInventoryIntelligence from "./CRMInventoryIntelligence";
 import "./index.css";
