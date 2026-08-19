83e0f1a715a8dfcafe0a39962e079cc7c6ab52d5
johnsonprince664-web <johnsonprince664@gmail.com>
2026-08-19 04:45:37 -0400

    Remove JARVIS chat route and keep voice-only dock

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CRMInventoryIntelligence from "./CRMInventoryIntelligence";
-import JarvisPortal from "./JarvisPortal";
 import JarvisDock from "./JarvisDock";
 import "./index.css";
 
-const params = new URLSearchParams(window.location.search);
-const jarvisMode = params.get("jarvis") === "1";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {jarvisMode ? (
-      <JarvisPortal />
-    ) : (
-      <>
-        <App />
-        <CRMInventoryIntelligence />
-        <JarvisDock />
-      </>
-    )}
+    <App />
+    <CRMInventoryIntelligence />
+    <JarvisDock />
   </React.StrictMode>
 );
